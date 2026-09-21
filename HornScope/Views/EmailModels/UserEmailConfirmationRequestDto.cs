f99edc955cbaf537685d8234060a55cabb03df6b
paramsingh <parampreet.digisoft@gmail.com>
2026-09-21 18:27:09 +0530

    Commit for fixing UJserEmailConfirmationRequestDto class issue

--- HornScope/Views/EmailModels/UserEmailConfirmationRequestDto.cs
+++ HornScope/Views/EmailModels/UserEmailConfirmationRequestDto.cs
@@ -2,9 +2,10 @@ namespace API.Views.EmailModels
 {
   public class UserEmailConfirmationRequestDto
   {
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string EmailConfirmationUrl { get; set; }
-  }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string EmailConfirmationUrl { get; set; } = string.Empty;
+        public string ApiUrl { get; set; } = string.Empty;
+    }
 }
